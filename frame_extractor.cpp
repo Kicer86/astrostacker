5feaa17ba092ebffd4888d206f975334025b9227
Michał Walenciak <michalwalenciak@gmail.com>
2024-06-19 22:24:10 +0200

    Use OpenMP to speedup stuff

--- frame_extractor.cpp
+++ frame_extractor.cpp
@@ -4,32 +4,75 @@ module;
 #include <chrono>
 #include <string_view>
 #include <opencv2/opencv.hpp>
+#include <omp.h>
 
 export module frame_extractor;
 
+namespace
+{
+    int64_t videoFrames(std::string_view file)
+    {
+        cv::VideoCapture video(std::string(file), cv::CAP_FFMPEG);
+
+        if (video.isOpened())
+        {
+            video.set(cv::CAP_PROP_POS_FRAMES, 0);
+            const auto frames = video.get(cv::CAP_PROP_FRAME_COUNT);
+            return frames;
+        }
+        else
+            return 0;
+    }
+}
 
 export void extractVideo(std::string_view file, std::string_view dir)
 {
-    cv::VideoCapture video(std::string(file), cv::CAP_FFMPEG);
+    const auto total_start = std::chrono::high_resolution_clock::now();
+    const auto frames = videoFrames(file);
 
-    if (video.isOpened())
+    #pragma omp parallel
     {
-        video.set(cv::CAP_PROP_POS_FRAMES, 0);
-        const auto frames = video.get(cv::CAP_PROP_FRAME_COUNT);
+        const auto threads = omp_get_num_threads();
+        const auto thread = omp_get_thread_num();
+        const auto group_size = frames > threads? frames / threads : 1;
 
-        for(double frame = 0; frame < frames; frame++)
+        #pragma omp master
         {
-            const auto start = std::chrono::high_resolution_clock::now();
+            std::cout << "Starting extraction with " << threads << " threads. Group size: " << group_size << "\n";
+        }
+        #pragma omp barrier
 
-            cv::Mat frameMat;
-            video >> frameMat;
+        cv::VideoCapture video(std::string(file), cv::CAP_FFMPEG);
+        if (video.isOpened())
+        {
+            const auto frames = static_cast<int64_t>(video.get(cv::CAP_PROP_FRAME_COUNT));
+            const auto first_frame = group_size * thread;
+            const auto last_frame = std::min(frames, first_frame + group_size);
 
-            cv::imwrite(std::format("{}/{}.tiff", dir, std::to_string(static_cast<int64_t>(frame))), frameMat);
-            const auto stop = std::chrono::high_resolution_clock::now();
-            const auto duration = stop - start;
-            const auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+            video.set(cv::CAP_PROP_POS_FRAMES, first_frame);
 
-            std::cout << "Processed frame #" << frame << ". Time: " << duration_ms.count() << "ms" << std::endl;
+            for(double frame = first_frame; frame < last_frame; frame++)
+            {
+                const auto start = std::chrono::high_resolution_clock::now();
+
+                cv::Mat frameMat;
+                video >> frameMat;
+
+                cv::imwrite(std::format("{}/{}.tiff", dir, std::to_string(static_cast<int64_t>(frame))), frameMat);
+                const auto stop = std::chrono::high_resolution_clock::now();
+                const auto duration = stop - start;
+                const auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+
+                std::stringstream thread_message;
+                thread_message << "Processed frame #" << frame << ". Time: " << duration_ms.count() << "ms\n";
+                std::cout << thread_message.str();
+            }
         }
     }
+
+    const auto total_stop = std::chrono::high_resolution_clock::now();
+    const auto duration = total_stop - total_start;
+    const auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
+
+    std::cout << "Total time: " << duration_ms.count() << "ms" << std::endl;
 }
