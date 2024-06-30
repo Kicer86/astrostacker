801271369f2222662c08c4c17595b377633f25d1
Michał Walenciak <michalwalenciak@gmail.com>
2024-06-30 11:30:40 +0200

    Restore median based stacker

--- images_stacker.cpp
+++ images_stacker.cpp
@@ -35,9 +35,10 @@ namespace
         return result;
     }
 
-    /*
+
     cv::Mat medianStacking(const std::vector<std::filesystem::path>& images)
     {
+        // TODO: rewrite with std::mdspan
         const cv::Mat firstImage = cv::imread(images.front());
         const auto imagesCount = images.size();
         std::vector<cv::Vec3b> pixels(imagesCount * firstImage.rows * firstImage.cols);
@@ -49,7 +50,7 @@ namespace
             const cv::Mat image = cv::imread(images[i]);
             for (int y = 0; y < image.rows; ++y)
                 for (int x = 0; x < image.cols; ++x)
-                    pixels[i * image.rows * image.cols + y * image.cols + x] = image.at<cv::Vec3b>(y, x);
+                    pixels[y * image.cols * imagesCount + x * imagesCount + i] = image.at<cv::Vec3b>(y, x);
         }
 
         cv::Mat result(firstImage.size(), firstImage.type());
@@ -59,7 +60,7 @@ namespace
         for (int y = 0; y < result.rows; ++y)
             for (int x = 0; x < result.cols; ++x)
             {
-                std::vector<cv::Vec3b>& px = pixels[y * result.cols + x];
+                const std::span<cv::Vec3b> px(&pixels[y * result.cols * imagesCount + x * imagesCount], imagesCount);
                 std::sort(px.begin(), px.end(), [](const cv::Vec3b& a, const cv::Vec3b& b)
                 {
                     return cv::norm(a) < cv::norm(b);
@@ -69,7 +70,6 @@ namespace
 
         return result;
     }
-    */
 }
 
 
@@ -80,10 +80,10 @@ export std::vector<std::filesystem::path> stackImages(const std::vector<std::fil
     const auto pathAvg = dir / "average.tiff";
     cv::imwrite(pathAvg, averageImg);
 
-    //const auto medianImg = medianStacking(images);
+    const auto medianImg = medianStacking(images);
 
-    //const auto pathMdn = dir / "median.tiff";
-    //cv::imwrite(pathMdn, medianImg);
+    const auto pathMdn = dir / "median.tiff";
+    cv::imwrite(pathMdn, medianImg);
 
-    return {pathAvg};
+    return {pathAvg, pathMdn};
 }
