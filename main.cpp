ffe3372176c757114bdff83b885137bd9c5456e1
Michał Walenciak <michalwalenciak@gmail.com>
2024-07-19 23:07:36 +0200

    Use boost command line parser

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <filesystem>
 
+#include <boost/program_options.hpp>
+
+
 import frame_extractor;
 import images_aligner;
 import images_enhancer;
@@ -38,14 +41,42 @@ namespace
 
 int main(int argc, char** argv)
 {
-    if (argc != 3)
+    namespace po = boost::program_options;
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("working-dir", po::value<std::string>(), "set working directory")
+        ("input-files", po::value<std::vector<std::string>>(), "input files");
+
+    po::variables_map vm;
+    po::positional_options_description p;
+    p.add("input-files", -1);
+    po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
     {
-        std::cerr << "Provide input video file and working dir as arguments\n";
+        std::cout << desc << "\n";
         return 1;
     }
 
-    const std::filesystem::path input_file = argv[1];
-    const std::filesystem::path wd = std::filesystem::path(argv[2]) / getCurrentTime();
+    if (vm.count("working-dir") == 0)
+    {
+        std::cerr << "--working-dir option is required\n";
+        return 1;
+    }
+
+    if (vm.count("input-files") == 0)
+    {
+        std::cerr << "Provide input files\n";
+        return 1;
+    }
+
+    const std::filesystem::path wd_option = vm["working-dir"].as<std::string>();
+    const std::vector<std::string> inputFiles = vm["input-files"].as<std::vector<std::string>>();
+    const std::filesystem::path input_file = inputFiles[0];
+    const std::filesystem::path wd = wd_option / getCurrentTime();
 
     std::filesystem::create_directory(wd);
 
