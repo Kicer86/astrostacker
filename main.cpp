07dd1c5a6d59b9e0f6606e2ca43b253b64b6130e
Michał Walenciak <michalwalenciak@gmail.com>
2024-07-27 18:42:59 +0200

    Catch all exceptions

--- main.cpp
+++ main.cpp
@@ -88,12 +88,17 @@ int main(int argc, char** argv)
     }
     catch(const std::invalid_argument& error)
     {
-        std::cout << "Error: " << error.what() << "\n";
+        std::cerr << "Error: " << error.what() << "\n";
         return 1;
     }
     catch(const std::logic_error& error)
     {
-        std::cout << "Error: " << error.what() << "\n";
+        std::cerr << "Error: " << error.what() << "\n";
+        return 1;
+    }
+    catch(...)
+    {
+        std::cerr << "Fail: Unhandled exception\n";
         return 1;
     }
 
