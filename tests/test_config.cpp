d5e5cbf783c3d9100de8f286eea6b303dff193ff
Michał Walenciak <michalwalenciak@gmail.com>
2024-08-16 16:45:21 +0200

    Add offset for crop

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -8,6 +8,7 @@
 
 import config;
 import images_picker;
+import utils;
 
 using testing::Contains;
 
@@ -27,4 +28,27 @@ TEST(ConfigTest, defaults)
     EXPECT_EQ(config.skip, 0);
     EXPECT_FALSE(config.split.has_value());
     EXPECT_TRUE(config.wd.string().starts_with("somedir"));
+}
+
+using CropParam = std::tuple<std::string_view, int, int, int, int>;
+
+class CropParserTest: public testing::TestWithParam<CropParam> { };
+
+INSTANTIATE_TEST_SUITE_P(
+    ValidCrop, CropParserTest,
+    testing::Values(
+        CropParam{ "100x200", 100, 200, 0, 0 },
+        CropParam{ "0x0,-5,-6", 0, 0, -5, -6 },
+        CropParam{ "1000x550,17,13", 1000, 550, 17, 13 }
+    )
+);
+
+
+TEST_P(CropParserTest, parsing)
+{
+    const auto& [input, w, h, x, y] = GetParam();
+
+    const auto result = Utils::readCrop(input);
+    ASSERT_TRUE(result);
+    EXPECT_EQ(*result, (std::tuple{w, h, x, y}));
 }
