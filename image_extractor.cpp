7688e8fe99d46c4cf7fae6e290535940a35115f6
Michał Walenciak <michalwalenciak@gmail.com>
2024-09-05 10:45:35 +0200

    Handle nef raw images

--- image_extractor.cpp
+++ image_extractor.cpp
@@ -4,6 +4,10 @@ module;
 #include <filesystem>
 #include <ranges>
 #include <vector>
+#include <boost/algorithm/string.hpp>
+#include <libraw/libraw.h>
+#include <opencv2/opencv.hpp>
+
 
 export module image_extractor;
 import utils;
@@ -46,8 +50,49 @@ export std::vector<std::filesystem::path> collectImages(const std::filesystem::p
     if (count < lastFrame)
         throw std::out_of_range("last frame > number of frames");
 
-    return Utils::processImages(std::ranges::subrange(images.begin() + firstFrame, images.begin() + lastFrame), dir, [](const auto& image)
+    std::vector<std::filesystem::path> result;
+    result.resize(lastFrame - firstFrame);
+
+    Utils::forEach(std::ranges::subrange(images.begin() + firstFrame, images.begin() + lastFrame), [&](const auto& i)
     {
-        return image;
+        const auto& path = images[i];
+        auto extension = path.extension().string();
+        boost::algorithm::to_lower(extension);
+
+        cv::Mat image;
+        const auto pathStr = path.string();
+
+        if (extension == ".nef")
+        {
+            LibRaw rawProcessor;
+
+            if (rawProcessor.open_file(pathStr.c_str()) != LIBRAW_SUCCESS)
+                throw std::runtime_error(std::format("Cannot open input file: {}", pathStr));
+
+            if (rawProcessor.unpack() != LIBRAW_SUCCESS)
+                throw std::runtime_error(std::format("Error when unpacking RAW file data: {}", pathStr));
+
+            rawProcessor.dcraw_process();
+            libraw_processed_image_t* rawImage = rawProcessor.dcraw_make_mem_image();
+            if (!rawImage)
+                throw std::runtime_error(std::format("No RAW image: {}", pathStr));
+
+            cv::Mat matImage(rawImage->height, rawImage->width, CV_8UC3, rawImage->data);
+            image = matImage.clone();
+
+            LibRaw::dcraw_clear_mem(rawImage);
+            rawProcessor.recycle();
+        }
+        else
+            image = cv::imread(pathStr);
+
+        const auto filename = path.stem();
+        const auto newLocation = dir / (filename.string() + ".png");
+
+        cv::imwrite(newLocation, image);
+
+        result[i] = newLocation;
     });
+
+    return result;
 }
